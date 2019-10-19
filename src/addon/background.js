52c973a6f7992ac66d3232539260f852b06714bb
museun <museun@outlook.com>
2019-10-19 02:08:11 -0400

    make addon not use the client, directly does an http post

--- src/addon/background.js
+++ src/addon/background.js
@@ -1,8 +1,12 @@
 var active_id = undefined;
+var port = "58810"; // TODO make this configuraable
 
 const timestamp = () => Math.round((new Date()).getTime() / 1000);
 
+// const set_port = user_port => { port = user_port; }
+
 const set_inactive = id => {
+    console.log(`setting inactive on tab: ${active_id}`);
     active_id = undefined;
     browser.browserAction.setIcon({ path: "icons/icon.svg", tabId: id });
     browser.tabs.onUpdated.removeListener(listener);
@@ -10,33 +14,46 @@ const set_inactive = id => {
 
 const set_active = id => {
     active_id = id;
+    console.log(`setting active on tab: ${active_id}`);
     browser.browserAction.setIcon({ path: "icons/icon-active.svg", tabId: id });
     const filter = { tabId: id }
     browser.tabs.onUpdated.addListener(listener, filter);
 }
 
-const log_url = (url, title) => {
+async function log_url(url) {
     let ts = timestamp();
-    console.log(ts, url, title);
-    browser.runtime.sendNativeMessage("whatsong", {
-        ts: ts,
-        url: url,
-        title: title,
-    }).then(msg => {
-        console.log(msg)
-    }, err => {
-        console.error(err)
-    });
+    console.log(`sending '${url}' @ '${ts}'`);
+    try {
+        let body = JSON.stringify({
+            kind: {
+                youtube: url,
+            },
+            ts: ts,
+            version: 1,
+        });
+        let resp = await fetch(`http://localhost:${port}/youtube`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: body
+        });
+        console.log(resp);
+    } catch (ex) {
+        console.error("error sending request: {}", ex);
+    }
 };
 
-function listener(id, info, tab) {
+async function listener(id, info, tab) {
+    console.log("listener was called");
     if (info.url) {
         browser.browserAction.setIcon({ path: "icons/icon-active.svg", tabId: id });
-        log_url(tab.url, tab.title);
+        await log_url(tab.url);
     }
+    return true;
 }
 
 browser.browserAction.onClicked.addListener(tab => {
+    console.log("button was clicked");
+
     if (active_id === tab.id) {
         console.log("setting inactive:", active_id);
         set_inactive(active_id);
